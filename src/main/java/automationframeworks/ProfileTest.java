b46fa12088e8db1b4b6a7afccbb3d9444b3bb223
menakajigi <menakajigi.babu@gmail.com]>
2025-03-06 10:58:24 -0500

    Added Profile Test Cases

---
+++ src/main/java/automationframeworks/ProfileTest.java
@@ -0,0 +1,10 @@
+package automationframeworks;
+
+public class ProfileTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Profile Test Cases Added");
+	}
+
+}
